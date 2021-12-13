3c8c9996238df24eca0d27d6b994cfa917e0b99b
WheatMillington <78046968+WheatMillington@users.noreply.github.com>
2021-12-14 10:14:08 +1300

    Update ControlPanel_2.h

--- els-f280049c/ControlPanel_2.h
+++ els-f280049c/ControlPanel_2.h
@@ -102,7 +102,7 @@ typedef union LED_REG_2
 
 struct KEY_BITS_2
 {
-    Uint16 KEY1:1;
+    Uint16 Zero:1;
     Uint16 KEY2:1;
     Uint16 KEY3:1;
     Uint16 KEY4:1;
@@ -153,7 +153,6 @@ private:
     // dummy register, for SPI
     Uint16 dummy;
 
-    void decomposeRPM(void);
     void decomposeValue(void);
     KEY_REG readKeys(void);
     Uint16 lcd_char(Uint16 x);
