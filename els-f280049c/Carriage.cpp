a9c542ae51da092e7f73b0322470b46482c73f65
WheatMillington <78046968+WheatMillington@users.noreply.github.com>
2022-01-13 10:29:47 +1300

    Update Carriage.cpp

--- els-f280049c/Carriage.cpp
+++ els-f280049c/Carriage.cpp
@@ -24,48 +24,20 @@
 // SOFTWARE.
 
 
-#include "StepperDrive.h"
+#include "Carriage.h"
 
-
-StepperDrive :: StepperDrive(void)
+int32 Carriage :: getCarriagePosition(void)
 {
-    //
-    // Set up global state variables
-    //
-    this->currentPosition = 0;
-    this->desiredPosition = 0;
-
-    //
-    // State machine starts at state zero
-    //
-    this->state = 0;
+    // Carriage position in hundreths of a mm
+    this->carriagePosition = (( (float) stepperDrive->currentPosition / (float) STEPPER_RESOLUTION) * LEADSCREW_HMM) + carriageOffset
+        
+    return carriagePosition;
 }
 
-void StepperDrive :: initHardware(void)
+void zeroCarriagePosition(void)
 {
-    //
-    // Configure GPIO pins for output
-    // GPIO0 = Step
-    // GPIO1 = Direction
-    // GPIO6 - Enable
-    // GPIO
-    //
-    EALLOW;
-    GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 0;
-    GpioCtrlRegs.GPAMUX1.bit.GPIO1 = 0;
-    GpioCtrlRegs.GPAMUX1.bit.GPIO6 = 0;
-    GpioCtrlRegs.GPAMUX1.bit.GPIO7 = 0;
-
-    GpioCtrlRegs.GPADIR.bit.GPIO0 = 1;
-    GpioCtrlRegs.GPADIR.bit.GPIO1 = 1;
-    GpioCtrlRegs.GPADIR.bit.GPIO6 = 1;
-    GpioCtrlRegs.GPADIR.bit.GPIO7 = 0; // input
-
-    GPIO_CLEAR_STEP;
-    GPIO_CLEAR_DIRECTION;
-    EDIS;
-
-    setEnabled(true);
+    // use an offset value to zero carriage position = will allow additional functionality in future
+    carriageOffset = -carriagePosition;
 }
 
 
